6ec3e2bc7d1c472819d7017e0a803d7804dbb47e
Nazmul Ahmed <nzml444@gmail.com>
2022-03-30 01:06:33 +0600

    1. App starting condition: Hide the dice

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
 
 const init = function () {
   // Initially, player's current and total scores are set to 0
@@ -11,5 +12,8 @@ const init = function () {
   score1El.textContent = 0;
   current0El.textContent = 0;
   current1El.textContent = 0;
+
+  // Hide the dice
+  diceEl.classList.add('hidden');
 };
 init();

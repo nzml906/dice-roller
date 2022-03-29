5938581a304f0ef6d1acc251039f0a1ed3f4c873
Nazmul Ahmed <nzml444@gmail.com>
2022-03-30 01:02:10 +0600

    1. App starting condition: Initially, player's current and total scores are set to 0

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const init = function () {
+  // Initially, player's current and total scores are set to 0
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+};
+init();
